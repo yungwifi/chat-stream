9efb31df0b62a8ca79649dcef3017d966bedc051
yungwifi <smerryman@me.com>
2018-05-30 15:48:31 -0400

    Heroku tests

---
+++ config/initializers/devise.rb
@@ -0,0 +1,3 @@
+Devise.setup do |config|
+    config.secret_key = '63dc5ffdc30cccc733488a50293b5e89b27f501872ff02baf96afb6ec5f9ceb82b02c6d5fdc213eb6b45f8ead7f4de3f9b42cc7faf245884f58f8f396d91b031'
+  end
