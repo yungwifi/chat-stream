d096df0bf275a27155a1f24dc5f15e63e5a72b07
yungwifi <smerryman@me.com>
2018-06-02 22:14:57 -0400

    somethings are working?

--- client/src/components/Cable.js
+++ client/src/components/Cable.js
@@ -1,20 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { ActionCable } from 'react-actioncable-provider';
 
-const Cable = () => {
-    return (
-        <Fragment>
-            {this.props.chat_room.map(chat_room => {
-                return (
-                    <ActionCable
-                        key={chat_room.id}
-                        channel={{ channel: 'PostsChannel', chat_room: chat_room.id }}
-                        onReceived={this.props.handleReceivedPost}
-                    />
-                );
-            })}
-        </Fragment>
-    );
+class Cable extends Component {
+    render() {
+        const chatroom = this.props.chat_room.map(chat_room => {
+            <div key={chat_room.id}> </div>
+        })
+        return (
+            <Fragment>
+                {this.props.chat_room.map(chat_room => {
+                    return (
+                        <ActionCable
+                            key={chatroom}
+                            channel={{ channel: 'PostsChannel', chat_room: chat_room.id }}
+                            onReceived={this.props.handleReceivedPost}
+                        />
+                    );
+                })}
+            </Fragment>
+        );
+    }
+
 };
 
 export default Cable;
