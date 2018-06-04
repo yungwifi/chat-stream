bcc2b78e3e19eb939d95b89e1ea97a3c4ac82d30
yungwifi <smerryman@me.com>
2018-06-04 14:00:24 -0400

    heroku see

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-ChatRoom.destroy_all
+User.destroy_all
 
 chatroom = ChatRoom.create!(title: 'main')
 
